5fbcecddc71d0397bd0174653fd1fbfbf1f0e05b
maria-zhynko <66383351+maria-zhynko@users.noreply.github.com>
2020-10-15 23:26:06 +0300

    fix: fix

--- src/index.js
+++ src/index.js
@@ -39,15 +39,10 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
-       let arr = [];
+       const arr = [];
 
     for (let i = 0; i < expr.length - 1; i += 10) {
-        arr.push(MORSE_TABLE[expr
-                .slice(i, i + 10)
-                .replace(/00/g, "")
-                .replace(/11/g, "-")
-                .replace(/10/g, ".")
-                .replace(/\*{10}/, " ")]);
+        arr.push(MORSE_TABLE[expr.slice(i, i + 10).replace(/00/g, "").replace(/11/g, "-").replace(/10/g, ".").replace(/\*{10}/, " ")]);
     }
     console.log(arr);
     return arr.join``;
